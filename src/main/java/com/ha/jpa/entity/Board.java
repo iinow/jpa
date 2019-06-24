3a7f923e8e29df34d283208e9f9613f804e59ea3
iinow <iinow@naver.com>
2019-06-24 22:48:44 +0900

    board 엔티티 추가

---
+++ src/main/java/com/ha/jpa/entity/Board.java
@@ -0,0 +1,59 @@
+package com.ha.jpa.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.TableGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+import lombok.Data;
+
+@Entity
+@Data
+@TableGenerator(
+		name = "BOARD_SEQ_GENERATOR",
+		table = "MY_SEQUENCES",
+		pkColumnName = "SEQUENCE_NAME",
+		pkColumnValue = "BOARD_SEQ",
+		allocationSize = 1)
+public class Board {
+	@Id
+	@GeneratedValue(strategy = GenerationType.TABLE, generator = "BOARD_SEQ_GENERATOR")
+	private Long id;
+	
+	@Enumerated(EnumType.STRING)
+	private BOARDTYPE type = BOARDTYPE.ADMIN;
+	
+	@Enumerated(EnumType.ORDINAL)
+	private BOARDTYPE value = BOARDTYPE.ADMIN;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date date = new Date();
+	
+	@Lob
+	private String lobString; //clob 으로 매핑 char[]
+	
+	@Lob
+	private byte[] lobByte;	//그외 blob 으로 매핑 byte[]
+	
+	@Transient
+	private String temp = "임시저장소";
+
+	public enum BOARDTYPE{
+		ADMIN(0), USER(1);
+		
+		public final int value;
+		
+		BOARDTYPE(int value) {
+			this.value = value;
+		}
+	}
+}
--- src/test/java/com/ha/jpa/JpaApplicationTests.java
+++ src/test/java/com/ha/jpa/JpaApplicationTests.java
@@ -1,16 +1,31 @@
 package com.ha.jpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.ha.jpa.entity.Academy;
+import com.ha.jpa.entity.Board;
+import com.ha.jpa.repository.BoardRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JpaApplicationTests {
+	@Autowired
+	EntityManagerFactory factory;
+	
+	@Autowired
+	BoardRepository repository;
 
 	@Test
 	public void contextLoads() {
+		Board board = new Board();
+		repository.save(board);
 	}
 
 }
