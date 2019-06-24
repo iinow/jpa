fbcba1f3295a503bd091c7916faf661f73e3be88
iinow <iinow@naver.com>
2019-06-24 21:52:34 +0900

    제약조건 추가

--- src/main/java/com/ha/jpa/entity/Academy.java
+++ src/main/java/com/ha/jpa/entity/Academy.java
@@ -1,10 +1,12 @@
 package com.ha.jpa.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -18,7 +20,10 @@ import lombok.ToString;
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Table(name = "academy")
+@Table(name = "academy", uniqueConstraints = {@UniqueConstraint(
+		name = "NAME_AGE_UNIQUE",
+		columnNames = {"NAME", "AGE"}
+)})
 @ToString
 @EqualsAndHashCode
 public class Academy {
@@ -27,6 +32,7 @@ public class Academy {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name", nullable = false, length = 10)
     private String name;
     private String address;
 
